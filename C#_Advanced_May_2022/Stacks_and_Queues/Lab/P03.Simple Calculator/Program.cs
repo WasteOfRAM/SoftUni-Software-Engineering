9204dbd24a6e5e33a614befeec17a1c47d732794
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-17 00:22:19 +0300

    Problems 3 and 4 solutions

---
+++ C#_Advanced_May_2022/Stacks_and_Queues/Lab/P03.Simple Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03.Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputSplit = Console.ReadLine().Split(' ');
+            Array.Reverse(inputSplit);
+
+            Stack<string> expresion = new Stack<string>(inputSplit);
+
+            int result = int.Parse(expresion.Pop());
+
+            while (expresion.Count > 0)
+            {
+                char sign = char.Parse(expresion.Pop());
+
+                if (sign == '+')
+                {
+                    result += int.Parse(expresion.Pop());
+                }
+                else
+                {
+                    result -= int.Parse(expresion.Pop());
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C#_Advanced_May_2022/Stacks_and_Queues/Lab/P04.Matching Brackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P04.Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expresion = Console.ReadLine();
+
+            Stack<int> bracketsIndexes = new Stack<int>();
+
+            for (int i = 0; i < expresion.Length; i++)
+            {
+                if (expresion[i] == '(')
+                {
+                    bracketsIndexes.Push(i);
+                }
+                else if (expresion[i] == ')')
+                {
+                    int startIndex = bracketsIndexes.Pop();
+                    int endIndex = i + 1;
+
+                    Console.WriteLine(expresion.Substring(startIndex, endIndex - startIndex));
+                }
+            }
+        }
+    }
+}
