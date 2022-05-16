e20d26950b4218d76d3e8f937371997e1b6e4184
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-17 00:46:37 +0300

    Problems 5 and 6 solutions

---
+++ C#_Advanced_May_2022/Stacks_and_Queues/Lab/P05.Print Even Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse);
+
+            Queue<int> queOfInts = new Queue<int>(input);
+
+            List<int> evenNums = new List<int>();
+
+            while (queOfInts.Count > 0)
+            {
+                int removedNum = queOfInts.Dequeue();
+
+                if (removedNum % 2 == 0)
+                {
+                    evenNums.Add(removedNum);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", evenNums));
+        }
+    }
+}
---
+++ C#_Advanced_May_2022/Stacks_and_Queues/Lab/P06.Supermarket/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06.Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> customerLine = new Queue<string>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                if (command == "Paid")
+                {
+                    foreach (var customer in customerLine)
+                    {
+                        Console.WriteLine(customer);
+                    }
+
+                    customerLine.Clear();
+                }
+                else
+                {
+                    customerLine.Enqueue(command);
+                }
+            }
+
+            Console.WriteLine($"{customerLine.Count} people remaining.");
+        }
+    }
+}
