466b519b76f2930e76f39c2a690e791fa8c72c25
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-16 23:43:48 +0300

    Problem two solution

---
+++ C#_Advanced_May_2022/Stacks_and_Queues/Lab/P02.Stack Sum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.Stack_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse);
+
+            Stack<int> stack = new Stack<int>(input);
+
+            string command;
+            while ((command = Console.ReadLine().ToLower()) != "end")
+            {
+                string[] cmdSplit = command.Split(' ');
+
+                if (cmdSplit[0].ToLower() == "add")
+                {
+                    stack.Push(int.Parse(cmdSplit[1]));
+                    stack.Push(int.Parse(cmdSplit[2]));
+                }
+                else if (cmdSplit[0].ToLower() == "remove")
+                {
+                    int countToRemove = int.Parse(cmdSplit[1]);
+
+                    if (countToRemove > stack.Count)
+                        continue;
+
+                    for (int i = 0; i < countToRemove; i++)
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
---
+++ C#_Advanced_May_2022/Stacks_and_Queues/README.md
@@ -0,0 +1,4 @@
+Problems descriptions and testing
+
+[Lab](https://judge.softuni.org/Contests/1445/Stacks-and-Queues-Lab)
+[EExercise](https://judge.softuni.org/Contests/1447/Stacks-and-Queues-Exercise)
