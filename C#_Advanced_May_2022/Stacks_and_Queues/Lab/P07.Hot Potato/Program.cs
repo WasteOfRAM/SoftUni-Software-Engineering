a568cb6f8576c311e5984f81fd750f368f62bb3a
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-17 22:00:13 +0300

    Problems 7 and 8 solutions

--- C#_Advanced_May_2022/Stacks_and_Queues/Lab/P07.Hot Potato/Program.cs
+++ C#_Advanced_May_2022/Stacks_and_Queues/Lab/P07.Hot Potato/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace P07.Hot_Potato
 {
@@ -6,7 +7,21 @@ namespace P07.Hot_Potato
     {
         static void Main(string[] args)
         {
-            
+            Queue<string> kids = new Queue<string>(Console.ReadLine().Split(' '));
+
+            int tosses = int.Parse(Console.ReadLine());
+
+            while (kids.Count > 1)
+            {
+                for (int i = 1; i < tosses; i++)
+                {
+                    kids.Enqueue(kids.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {kids.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {kids.Peek()}");
         }
     }
 }
---
+++ C#_Advanced_May_2022/Stacks_and_Queues/Lab/P08.Traffic Jam/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P08.Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsPassing = int.Parse(Console.ReadLine());
+
+            Queue<string> trafficStop = new Queue<string>();
+
+            int carsPassed = 0;
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 1; i <= carsPassing; i++)
+                    {
+                        if (trafficStop.Count == 0)
+                        {
+                            break;
+                        }
+
+                        Console.WriteLine($"{trafficStop.Dequeue()} passed!");
+                        carsPassed++;
+                    }
+                }
+                else
+                {
+                    trafficStop.Enqueue(command);
+                }
+            }
+
+            Console.WriteLine($"{carsPassed} cars passed the crossroads.");
+        }
+    }
+}
