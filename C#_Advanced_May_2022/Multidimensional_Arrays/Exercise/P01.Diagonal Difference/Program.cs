f1d4c65514363f8b6085a7d0e81665e116558cf4
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-20 21:49:53 +0300

    Problems 1 and 2 solutions

--- C#_Advanced_May_2022/Multidimensional_Arrays/Exercise/P01.Diagonal Difference/Program.cs
+++ C#_Advanced_May_2022/Multidimensional_Arrays/Exercise/P01.Diagonal Difference/Program.cs
@@ -6,7 +6,30 @@ namespace P01.Diagonal_Difference
     {
         static void Main()
         {
-            
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[matrixSize, matrixSize];
+
+            for (int row = 0; row < matrixSize; row++)
+            {
+                string inputData = Console.ReadLine();
+                for (int col = 0; col < matrixSize; col++)
+                {
+                    matrix[row, col] = int.Parse(inputData.Split()[col]);
+                }
+            }
+
+            int primaryDiagonalSum = 0;
+            int secondaryDiagonalSum = 0;
+
+            for (int i = 0; i < matrixSize; i++)
+            {
+                primaryDiagonalSum += matrix[i, i];
+
+                secondaryDiagonalSum += matrix[(matrixSize - 1) - i, i];
+            }
+
+            Console.WriteLine(Math.Abs(primaryDiagonalSum - secondaryDiagonalSum));
         }
     }
 }
---
+++ C#_Advanced_May_2022/Multidimensional_Arrays/Exercise/P02.Squares in Matrix/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace P02.Squares_in_Matrix
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string matrixSize = Console.ReadLine();
+            int rows = int.Parse(matrixSize.Split()[0]);
+            int columns = int.Parse(matrixSize.Split()[1]);
+
+            char[,] matrix = new char[rows, columns];
+
+            int totalEqualSquares = 0;
+
+            MatrixFill(matrix);
+
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1) - 1; col++)
+                {
+                    char mainChar = matrix[row, col];
+                    char rightChar = matrix[row, col + 1];
+                    char bottomChar = matrix[row + 1, col];
+                    char diagonalChar = matrix[row + 1, col + 1];
+
+                    if (rightChar == mainChar && bottomChar == mainChar && diagonalChar == mainChar)
+                    {
+                        totalEqualSquares++;
+                    }
+                }
+            }
+
+            Console.WriteLine(totalEqualSquares);
+        }
+
+        private static void MatrixFill(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] matrixRow = Console.ReadLine().Split();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = char.Parse(matrixRow[col]);
+                }
+            }
+        }
+    }
+}
