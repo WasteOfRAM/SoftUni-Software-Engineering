e78bcb31f6c48e81096c94d1569a03486e9fa022
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-22 20:17:21 +0300

    Problem 10 solution

--- C#_Advanced_May_2022/Stacks_and_Queues/Exercise/P10.Crossroads/Program.cs
+++ C#_Advanced_May_2022/Stacks_and_Queues/Exercise/P10.Crossroads/Program.cs
@@ -10,11 +10,64 @@ namespace P10.Crossroads
             int greenLightDuration = int.Parse(Console.ReadLine());
             int freeWindowDuration = int.Parse(Console.ReadLine());
 
-            int totalCarsPassed = 0;
-
             Queue<string> carsQueue = new Queue<string>();
 
-            
+            string curentCar = string.Empty;
+            int hitIndex = 0;
+            bool crash = false;
+
+            int totalCarsPassed = 0;
+
+            string command;
+            while ((command = Console.ReadLine()) != "END" && !crash)
+            {
+                if (command != "green")
+                {
+                    carsQueue.Enqueue(command);
+                }
+                else
+                {
+                    int curentGreenLight = greenLightDuration;
+                    int timeLeftToPass = freeWindowDuration;
+
+                    while (curentGreenLight > 0 && carsQueue.Count > 0)
+                    {
+                        curentCar = carsQueue.Dequeue();
+                        curentGreenLight -= curentCar.Length;
+
+                        if (curentGreenLight > 0)
+                        {
+                            totalCarsPassed++;
+                        }
+                        else
+                        {
+                            timeLeftToPass += curentGreenLight;
+
+                            if (timeLeftToPass < 0)
+                            {
+                                crash = true;
+                                hitIndex = curentCar.Length + timeLeftToPass;
+                                break;
+                            }
+                            else
+                            {
+                                totalCarsPassed++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (crash)
+            {
+                Console.WriteLine("A crash happened!");
+                Console.WriteLine($"{curentCar} was hit at {curentCar[hitIndex]}.");
+            }
+            else
+            {
+                Console.WriteLine("Everyone is safe.");
+                Console.WriteLine($"{totalCarsPassed} total cars passed the crossroads.");
+            }
         }
     }
 }
