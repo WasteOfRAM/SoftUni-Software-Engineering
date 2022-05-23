dee42dc523512a8ea10c8c0469ef03f585233214
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-23 22:46:57 +0300

    Problems 3 and 4 solutions

---
+++ C#_Advanced_May_2022/Sets_and_Dictionaries_Advanced/Lab/P03.Largest 3 Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).OrderByDescending(n => n).ToArray().Take(3);
+
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
---
+++ C#_Advanced_May_2022/Sets_and_Dictionaries_Advanced/Lab/P04.Product Shop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04.Product_Shop
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var stores = new Dictionary<string, Dictionary<string, double>>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Revision")
+            {
+                string storeName = input.Split(", ")[0];
+                string product = input.Split(", ")[1];
+                double productPrice = double.Parse(input.Split(",")[2]);
+
+                if(!stores.ContainsKey(storeName))
+                    stores[storeName] = new Dictionary<string, double>();
+
+                if(!stores[storeName].ContainsKey(product))
+                    stores[storeName][product] = productPrice;
+
+                stores[storeName][product] = productPrice;
+            }
+
+            foreach (var store in stores.OrderBy(s => s.Key))
+            {
+                Console.WriteLine(store.Key + "->");
+                foreach (var product in store.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
