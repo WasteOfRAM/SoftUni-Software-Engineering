2fcd2cf814b33988efc8077ea13ed976d9b12339
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-23 23:26:02 +0300

    Problems 7 and 8 solutions

--- C#_Advanced_May_2022/Sets_and_Dictionaries_Advanced/Lab/P07.Parking Lot/Program.cs
+++ C#_Advanced_May_2022/Sets_and_Dictionaries_Advanced/Lab/P07.Parking Lot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace P07.Parking_Lot
 {
@@ -6,7 +7,24 @@ namespace P07.Parking_Lot
     {
         static void Main()
         {
-            
+            HashSet<string> parkingLot = new HashSet<string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string direction = input.Split(", ")[0];
+                string plateNumber = input.Split(", ")[1];
+
+                if(direction == "IN")
+                    parkingLot.Add(plateNumber);
+                else
+                    parkingLot.Remove(plateNumber);
+            }
+
+            if(parkingLot.Count == 0)
+                Console.WriteLine("Parking Lot is Empty");
+            else
+                Console.WriteLine(string.Join(Environment.NewLine, parkingLot));
         }
     }
 }
---
+++ C#_Advanced_May_2022/Sets_and_Dictionaries_Advanced/Lab/P08.SoftUni Party/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P08.SoftUni_Party
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            HashSet<string> guestList = new HashSet<string>();
+
+            while (true)
+            {
+                string reservationNumber = Console.ReadLine();
+
+                if(reservationNumber == "PARTY")
+                    break;
+
+                guestList.Add(reservationNumber);
+            }
+
+            while (true)
+            {
+                string arrivals = Console.ReadLine();
+
+                if(arrivals == "END")
+                {
+                    Console.WriteLine(guestList.Count);
+                    Console.WriteLine(string.Join(Environment.NewLine, guestList.OrderByDescending(code => char.IsDigit(code[0]))));
+                    break;
+                }
+
+                if (guestList.Contains(arrivals))
+                    guestList.Remove(arrivals);
+            }
+        }
+    }
+}
