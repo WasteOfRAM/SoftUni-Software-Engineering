f6ca54a0a4596ae38bc1e7e9447668dce68c59ef
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-23 22:00:36 +0300

    Sets and dictionaries setup and problem 1 solution

---
+++ C#_Advanced_May_2022/Sets_and_Dictionaries_Advanced/Lab/P01.Count Same Values in Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var numbersCount = new Dictionary<double, int>();
+
+            double[] numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            foreach (var num in numbers)
+            {
+                if (!numbersCount.ContainsKey(num))
+                    numbersCount[num] = 1;
+                else
+                    numbersCount[num]++;
+            }
+
+            foreach (var (number, count) in numbersCount)
+            {
+                Console.WriteLine($"{number} - {count} times");
+            }
+        }
+    }
+}
---
+++ C#_Advanced_May_2022/Sets_and_Dictionaries_Advanced/README.md
@@ -0,0 +1,5 @@
+### Problems descriptions and testing  
+
+Needs a registration to view.  
+[Lab](https://judge.softuni.org/Contests/1465/Sets-and-Dictionaries-Advanced-Lab)  
+[Exercise](https://judge.softuni.org/Contests/1466)
