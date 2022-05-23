1ea90b5b622e788573e87e81b3df51ec6fad7cb1
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-23 22:25:28 +0300

    Problem 2 solution

---
+++ C#_Advanced_May_2022/Sets_and_Dictionaries_Advanced/Lab/P02.Average Student Grades/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02.Average_Student_Grades
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            var studentGrades = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < studentsCount; i++)
+            {
+                string[] curentStudent = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if(!studentGrades.ContainsKey(curentStudent[0]))
+                    studentGrades[curentStudent[0]] = new List<decimal>();
+
+                studentGrades[curentStudent[0]].Add(decimal.Parse(curentStudent[1]));
+            }
+
+            foreach (var (name, grades) in studentGrades)
+            {
+                Console.WriteLine($"{name} -> {string.Join(' ',grades.Select(grade => grade.ToString("f2")))} (avg: {grades.Average():f2})");
+            }
+        }
+    }
+}
