33172ad6d34c7231d1ce46951c5f6552f064541e
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-24 22:30:47 +0300

    Problems 3 and 4 solutions

---
+++ C#_Advanced_May_2022/Sets_and_Dictionaries_Advanced/Exercise/P03.Periodic Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03.Periodic_Table
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int inputLines = int.Parse(Console.ReadLine());
+
+            var elements = new HashSet<string>();
+
+            for (int i = 0; i < inputLines; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var elem in line)
+                {
+                    elements.Add(elem);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', elements.OrderBy(e => e)));
+        }
+    }
+}
---
+++ C#_Advanced_May_2022/Sets_and_Dictionaries_Advanced/Exercise/P04.Even Times/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P04.Even_Times
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                if(numbers.ContainsKey(input))
+                    numbers[input]++;
+                else
+                    numbers[input] = 1;
+            }
+
+            int num = numbers.FirstOrDefault(n => n.Value % 2 == 0).Key;
+
+            Console.WriteLine(num);
+        }
+    }
+}
