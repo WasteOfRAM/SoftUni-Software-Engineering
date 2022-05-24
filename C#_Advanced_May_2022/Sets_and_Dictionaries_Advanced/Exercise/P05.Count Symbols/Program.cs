5a833a70420869962fe8f6cf7ed7f526ee8100ed
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-24 23:15:22 +0300

    Problems 5, 6 solutions

---
+++ C#_Advanced_May_2022/Sets_and_Dictionaries_Advanced/Exercise/P05.Count Symbols/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05.Count_Symbols
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+
+            Dictionary<char, int> characters = new Dictionary<char, int>();
+
+            foreach (var ch in text)
+            {
+                if(characters.ContainsKey(ch))
+                    characters[ch]++;
+                else
+                    characters[ch] = 1;
+            }
+
+            foreach (var character in characters.OrderBy(n => n.Key))
+            {
+                Console.WriteLine($"{character.Key}: {character.Value} time/s");
+            }
+        }
+    }
+}
---
+++ C#_Advanced_May_2022/Sets_and_Dictionaries_Advanced/Exercise/P06.Wardrobe/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P06.Wardrobe
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int inputLines = int.Parse(Console.ReadLine());
+
+            var wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+            string[] delimiters = new string[] { " -> ", "," };
+
+            for (int i = 0; i < inputLines; i++)
+            {
+                List<string> inputLine = Console.ReadLine().Split(delimiters, StringSplitOptions.RemoveEmptyEntries).ToList();
+                string color = inputLine[0];
+                inputLine.RemoveAt(0);
+
+                if(!wardrobe.ContainsKey(color))
+                    wardrobe[color] = new Dictionary<string, int>();
+
+                foreach (var item in inputLine)
+                {
+                    if(!wardrobe[color].ContainsKey(item))
+                        wardrobe[color][item] = 1;
+                    else
+                        wardrobe[color][item]++;
+                }
+            }
+
+            string lookingForItem = Console.ReadLine();
+            string itemColor = lookingForItem.Split(' ')[0];
+            string itemType = lookingForItem.Split(' ')[1];
+
+            foreach (var (color, items) in wardrobe)
+            {
+                Console.WriteLine($"{color} clothes:");
+                foreach (var (item, itemCount) in items)
+                {
+                    Console.WriteLine((color == itemColor && item == itemType) ? $"* {item} - {itemCount} (found!)" : $"* {item} - {itemCount}");
+                }
+            }
+
+        }
+    }
+}
