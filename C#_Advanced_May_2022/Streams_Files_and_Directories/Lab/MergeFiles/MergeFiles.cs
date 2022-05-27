4eb749fbb01e9aab23c11f7e8ba4626bbff5989a
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-27 23:49:15 +0300

    Problem 4 solution

--- C#_Advanced_May_2022/Streams_Files_and_Directories/Lab/MergeFiles/MergeFiles.cs
+++ C#_Advanced_May_2022/Streams_Files_and_Directories/Lab/MergeFiles/MergeFiles.cs
@@ -15,7 +15,29 @@
 
         public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
         {
-            
+            using (StreamReader readerInput1 = new StreamReader(firstInputFilePath), readerInput2 = new StreamReader(secondInputFilePath))
+            {
+                string line1 = readerInput1.ReadLine();
+                string line2 = readerInput2.ReadLine();
+                using (var writer = new StreamWriter(outputFilePath))
+                {
+                    while (true)
+                    {
+                        if (line1 == null && line2 == null)
+                            break;
+
+                        if (line1 != null)
+                            writer.WriteLine(line1);
+
+                        if (line2 != null)
+                            writer.WriteLine(line2);
+
+
+                        line1 = readerInput1.ReadLine();
+                        line2 = readerInput2.ReadLine();
+                    }
+                }
+            }
         }
     }
 }
