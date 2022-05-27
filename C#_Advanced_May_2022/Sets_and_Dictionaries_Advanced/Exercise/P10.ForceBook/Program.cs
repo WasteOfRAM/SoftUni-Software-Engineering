1a17b267e672468de20ac88eaf1def04a494436c
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-27 21:30:33 +0300

    Problem 10 solution

---
+++ C#_Advanced_May_2022/Sets_and_Dictionaries_Advanced/Exercise/P10.ForceBook/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P10.ForceBook
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var forceBook = new Dictionary<string, SortedSet<string>>();
+
+            string[] delimiters = new string[] { " | ", " -> " };
+
+            string input;
+            while ((input = Console.ReadLine()) != "Lumpawaroo")
+            {
+                string[] inputSplit = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                string forceSide = string.Empty;
+                string forceUser = string.Empty;
+
+                if (input.Contains("|"))
+                {
+                    forceSide = inputSplit[0];
+                    forceUser = inputSplit[1];
+
+                    if (!forceBook.Any(x => x.Value.Contains(forceUser)))
+                        if (!forceBook.ContainsKey(forceSide))
+                            forceBook[forceSide] = new SortedSet<string>() { forceUser };
+                        else if (!forceBook[forceSide].Contains(forceUser))
+                            forceBook[forceSide].Add(forceUser);
+                }
+                else
+                {
+                    forceSide = inputSplit[1];
+                    forceUser = inputSplit[0];
+
+                    if (!forceBook.ContainsKey(forceSide))
+                        forceBook[forceSide] = new SortedSet<string>();
+
+                    foreach (var side in forceBook)
+                    {
+                        side.Value.Remove(forceUser);
+                    }
+
+                    forceBook[forceSide].Add(forceUser);
+
+                    Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                }
+            }
+
+            foreach (var side in forceBook.OrderByDescending(s => s.Value.Count).ThenBy(s => s.Key))
+            {
+                if (side.Value.Count > 0)
+                    Console.WriteLine($"Side: {side.Key}, Members: {side.Value.Count}");
+
+                foreach (var user in side.Value)
+                {
+                    Console.WriteLine($"! {user}");
+                }
+            }
+        }
+    }
+}
