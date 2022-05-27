fc58f0d6b9c5d2c07ad6828f7a8febfaa8c375d4
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-27 23:34:00 +0300

    Problem 3 solution

--- C#_Advanced_May_2022/Streams_Files_and_Directories/Lab/WordCount/WordCount.cs
+++ C#_Advanced_May_2022/Streams_Files_and_Directories/Lab/WordCount/WordCount.cs
@@ -17,6 +17,62 @@
 
         public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
         {
+            var wordsCount = new List<Word>();
+
+            string[] test = new string[] { "ewrewr", "rfhdth", "Asdfsf" };
+
+            using (var reader = new StreamReader(wordsFilePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (var word in words)
+                    {
+                        wordsCount.Add(new Word(word));
+                    }
+                }
+            }
+
+            using (var reader = new StreamReader(textFilePath))
+            {
+                char[] delimiters = new char[] { '.', '?', '!', ' ', ';', ':', ',', '-' };
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] lineSplit = line.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (var word in wordsCount)
+                    {
+                        var matchCount = lineSplit.Where(w => w.Equals(word.WordToCount, StringComparison.InvariantCultureIgnoreCase)).ToArray();
+                        word.Count += matchCount.Length;
+                    }
+                }
+            }
+
+            using (var writer = new StreamWriter(outputFilePath))
+            {
+                foreach (var word in wordsCount.OrderByDescending(w => w.Count))
+                {
+                    writer.WriteLine($"{word.WordToCount} - {word.Count}");
+                }
+            }
+
+            
         }
     }
+
+    class Word
+    {
+        public Word(string word)
+        {
+            this.WordToCount = word;
+        }
+
+        public string WordToCount { get; set; }
+        public int Count { get; set; } = 0;
+    }
+    
 }
