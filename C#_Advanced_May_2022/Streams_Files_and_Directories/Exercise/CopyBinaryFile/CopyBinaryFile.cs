151089675d68b41e7649b4b2dba6fda6ec492d5f
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-28 22:12:00 +0300

    Exercises setup and problems 1, 2 and 3  solutions

---
+++ C#_Advanced_May_2022/Streams_Files_and_Directories/Exercise/CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,29 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using FileStream stream = new FileStream(inputFilePath, FileMode.Open);
+
+            byte[] buffer = new byte[stream.Length];
+
+            stream.Read(buffer, 0, buffer.Length);
+
+            using FileStream streamWriter = new FileStream(outputFilePath, FileMode.Create);
+
+            streamWriter.Write(buffer, 0, buffer.Length);
+        }
+    }
+}
---
+++ C#_Advanced_May_2022/Streams_Files_and_Directories/Exercise/EvenLines/EvenLines.cs
@@ -0,0 +1,50 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            using StreamReader reader = new StreamReader(inputFilePath);
+
+            List<string> procesedLines = new List<string>();
+
+            int lineIndex = 0;
+
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (lineIndex % 2 == 0)
+                {
+                    string reversedLine = line.Replace('-', '@')
+                                              .Replace(',', '@')
+                                              .Replace('.', '@')
+                                              .Replace('!', '@')
+                                              .Replace('?', '@');
+
+                    string[] lineSplit = reversedLine.Split();
+
+                    Array.Reverse(lineSplit);
+
+                    reversedLine = string.Join(' ', lineSplit);
+                    procesedLines.Add(reversedLine);
+
+                }
+
+                lineIndex++;
+            }
+
+            return string.Join(Environment.NewLine, procesedLines);
+        }
+    }
+}
