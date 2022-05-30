1e0edfb51f25b75f80c9ce47b2704ca5b45ba291
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-30 22:12:40 +0300

    Problems 2 and 3 solutions

---
+++ C#_Advanced_May_2022/Functional_Programming/Lab/P02.Sum Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace P02.Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
---
+++ C#_Advanced_May_2022/Functional_Programming/Lab/P03.Count Uppercase Words/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Func<string, bool> iCapitalLetter = str => str.Length > 0 && char.IsUpper(str[0]);
+
+            string[] words = Console.ReadLine().Split(' ').Where(iCapitalLetter).ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
