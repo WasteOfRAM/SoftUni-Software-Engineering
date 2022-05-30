956e9fee8cd678dd5139480bf7ecd184864b4960
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-05-30 21:43:34 +0300

    Setup and problem 1 solution

---
+++ C#_Advanced_May_2022/Functional_Programming/Lab/P01.Sort Even Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace P01.Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).Where(n => n % 2 == 0).OrderBy(n => n).ToArray();
+
+            Console.WriteLine(string.Join(", " , numbers));
+        }
+    }
+}
---
+++ C#_Advanced_May_2022/Functional_Programming/README.md
@@ -0,0 +1,5 @@
+### Problems descriptions and testing  
+
+Needs a registration to view.  
+[Lab](https://judge.softuni.org/Contests/1472/Functional-Programming-Lab)  
+[Exercise](https://judge.softuni.org/Contests/1473/Functional-Programming-Exercise)
