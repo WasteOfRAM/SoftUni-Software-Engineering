79bc4b6a778117ca010a1227d947926855bf4d5d
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-06-02 23:40:56 +0300

    Problem 6 solution

---
+++ C#_Advanced_May_2022/Defining_Classes/Exercise/P06.Speed_Racing/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    internal class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelRatePerKilometer;
+        private double travelledDistance;
+
+        public string Model { get => this.model; set => this.model = value; }
+        public double FuelAmount { get => this.fuelAmount; set => this.fuelAmount = value; }
+        public double FuelRatePerKilometer { get => this.fuelRatePerKilometer; set => this.fuelRatePerKilometer = value; }
+
+        public Car(string model, double fuelAmount, double fuelRatePerKilometer)
+        {
+            this.model = model;
+            this.fuelAmount = fuelAmount;
+            this.fuelRatePerKilometer = fuelRatePerKilometer;
+
+            this.travelledDistance = 0;
+        }
+
+        public void Drive(int distance)
+        {
+            double fuelNeeded = this.fuelRatePerKilometer * distance;
+
+            if (fuelNeeded <= this.fuelAmount)
+            {
+                this.fuelAmount -= fuelNeeded;
+                this.travelledDistance += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{this.model} {this.fuelAmount:f2} {this.travelledDistance}");
+        }
+    }
+}
---
+++ C#_Advanced_May_2022/Defining_Classes/Exercise/P06.Speed_Racing/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            List<Car> cars = new List<Car>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string model = input.Split()[0];
+                double fuelAmout = double.Parse(input.Split()[1]);
+                double fuelRate = double.Parse(input.Split()[2]);
+
+                cars.Add(new Car(model, fuelAmout, fuelRate));
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string model = command.Split()[1];
+                int distance = int.Parse(command.Split()[2]);
+
+                cars.First(car => car.Model == model).Drive(distance);
+            }
+
+            cars.ForEach(car => car.Print());
+        }
+    }
+}
