a41bb0d16e133930dc50132d711299dc4356b7a0
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-06-07 23:34:12 +0300

    Custom list inplementation

---
+++ C#_Advanced_May_2022/Implementing_Stack_and_Queue/Lab/Create_Custom_Data_Structures/MyList.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_Custom_Data_Structures
+{
+    public class MyList
+    {
+        private const int INITIAL_CAPACITY = 4;
+        private int[] data;
+
+        public int Count { get; private set; }
+        public int this[int index]
+        {
+            get { ValidatingIndex(index); return data[index]; }
+            set { ValidatingIndex(index); data[index] = value; }
+        }
+
+
+        public MyList()
+            : this(INITIAL_CAPACITY)
+        {
+
+        }
+
+        public MyList(int capacity)
+        {
+            this.data = new int[capacity];
+            this.Count = 0;
+        }
+
+        public void Add(int element)
+        {
+            if (this.Count == this.data.Length)
+            {
+                Resize();
+            }
+
+            data[this.Count] = element;
+            this.Count++;
+        }
+
+        public void Insert(int index, int element)
+        {
+            ValidatingIndex(index);
+
+            if (this.Count == this.data.Length)
+            {
+                Resize();
+            }
+
+            ShiftRight(index);
+            this.data[index] = element;
+            this.Count++;
+        }
+
+        public int RemoveAt(int index)
+        {
+            ValidatingIndex(index);
+            var element = this.data[index];
+            this.data[index] = default(int);
+            Shift(index);
+            this.Count--;
+
+            if (this.Count <= this.data.Length / 2)
+                Shrink();
+
+            return element;
+        }
+
+        public bool Constains(int element)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this.data[i] == element)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            ValidatingIndex(firstIndex);
+            ValidatingIndex(secondIndex);
+
+            int firstElemValue = this.data[firstIndex];
+            this.data[firstIndex] = this.data[secondIndex];
+            this.data[secondIndex] = firstElemValue;
+        }
+
+        private void Shift(int index)
+        {
+            for (int i = index; i < this.Count - 1; i++)
+            {
+                this.data[i] = this.data[i + 1];
+            }
+        }
+
+        private void ShiftRight(int index)
+        {
+            for (int i = this.Count; i > index; i--)
+            {
+                this.data[i] = this.data[i - 1];
+            }
+        }
+
+        private void Resize() => this.data = ChageArraySize("*");
+        private void Shrink() => this.data = ChageArraySize("/");
+
+        private int[] ChageArraySize(string operation)
+        {
+            int loops;
+            int newSize = this.data.Length;
+
+            if (operation == "*")
+            {
+                newSize = this.data.Length * 2;
+                loops = this.data.Length;
+            }
+            else
+            {
+                if (this.data.Length / 2 > 2)
+                    newSize = this.data.Length / 2;
+
+                loops = newSize;
+            }
+
+            var newData = new int[newSize];
+
+            for (int i = 0; i < loops; i++)
+            {
+                newData[i] = this.data[i];
+            }
+
+            return newData;
+        }
+
+        private void ValidatingIndex(int index)
+        {
+            if (index >= 0 && index < this.Count)
+                return;
+
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+}
---
+++ C#_Advanced_May_2022/Implementing_Stack_and_Queue/Lab/Create_Custom_Data_Structures/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using Create_Custom_Data_Structures;
+
+var list = new MyList();
+
+list.Add(1);
+list.Add(2);
+list.Add(3);
+list.Add(4);
+list.Add(5);
+
+Console.WriteLine(list.Count);
+
+Console.WriteLine(list.RemoveAt(0));
+Console.WriteLine(list.RemoveAt(0));
+Console.WriteLine(list.RemoveAt(0));
+Console.WriteLine(list.RemoveAt(0));
+Console.WriteLine(list.RemoveAt(0));
+
+for (int i = 1; i <= 10; i++)
+{
+    list.Add(i);
+}
+
+list.Insert(7, 16);
+
+list.Swap(7, 1);
+
+var contains = list.Constains(654);
+
+for (int i = 0; i < list.Count; i++)
+{
+    Console.WriteLine(list[i]);
+}
+
+Console.WriteLine();
+
+Console.WriteLine(contains);
