30b0930f2f716727b2a74be1a13793c1b9aa0213
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-06-08 00:13:24 +0300

    Custom Stack

--- C#_Advanced_May_2022/Implementing_Stack_and_Queue/Lab/Create_Custom_Data_Structures/CustomStack.cs
+++ C#_Advanced_May_2022/Implementing_Stack_and_Queue/Lab/Create_Custom_Data_Structures/CustomStack.cs
@@ -8,6 +8,92 @@ namespace Create_Custom_Data_Structures
 {
     internal class CustomStack
     {
+        private const int INITIAL_CAPACITY = 4;
+        private int[] data;
 
+        public int Count { get; private set; }
+
+        public CustomStack()
+            :this(INITIAL_CAPACITY)
+        {
+
+        }
+
+        public CustomStack(int size)
+        {
+            this.data = new int[size];
+            this.Count = 0;
+        }
+
+        public void Push(int element)
+        {
+            if (this.Count == this.data.Length)
+                Resize();
+
+            this.data[this.Count++] = element;
+        }
+
+        public int Pop()
+        {
+            if (this.Count == 0)
+                throw new InvalidOperationException("CustomStack is empty");
+
+            int lastElement = this.data[this.Count - 1];
+            this.Count--;
+
+            if (this.Count <= this.data.Length / 2)
+                Shrink();
+
+            return lastElement;
+        }
+
+        public int Peek()
+        {
+            if (this.Count == 0)
+                throw new InvalidOperationException("CustomStack is empty");
+
+            int lastElement = this.data[this.Count - 1];
+
+            return lastElement;
+        }
+
+        public void ForEach(Action<object> action)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                action(this.data[i]);
+            }
+        }
+
+        private void Resize() => this.data = ChageArraySize("*");
+        private void Shrink() => this.data = ChageArraySize("/");
+
+        private int[] ChageArraySize(string operation)
+        {
+            int loops;
+            int newSize = this.data.Length;
+
+            if (operation == "*")
+            {
+                newSize = this.data.Length * 2;
+                loops = this.data.Length;
+            }
+            else
+            {
+                if (this.data.Length / 2 > 2)
+                    newSize = this.data.Length / 2;
+
+                loops = newSize;
+            }
+
+            var newData = new int[newSize];
+
+            for (int i = 0; i < loops; i++)
+            {
+                newData[i] = this.data[i];
+            }
+
+            return newData;
+        }
     }
 }
--- C#_Advanced_May_2022/Implementing_Stack_and_Queue/Lab/Create_Custom_Data_Structures/StartUp.cs
+++ C#_Advanced_May_2022/Implementing_Stack_and_Queue/Lab/Create_Custom_Data_Structures/StartUp.cs
@@ -1,3 +1,16 @@
 ﻿using Create_Custom_Data_Structures;
 
 var stack = new CustomStack();
+
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+stack.Push(4);
+stack.Push(5);
+
+stack.Pop();
+stack.Pop();
+
+Console.WriteLine(stack.Peek());
+
+stack.ForEach(x => Console.WriteLine(x));
