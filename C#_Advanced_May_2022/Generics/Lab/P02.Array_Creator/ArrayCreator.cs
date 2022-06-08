65c294af075283b61b9c15aeb39a1a2dfb1def71
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-06-08 21:13:14 +0300

    Problems 2 and 3 solutions

---
+++ C#_Advanced_May_2022/Generics/Lab/P02.Array_Creator/ArrayCreator.cs
@@ -0,0 +1,18 @@
+﻿
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int lenght, T item)
+        {
+            var data = new T[lenght];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = item;
+            }
+
+            return data;
+        }
+    }
+}
