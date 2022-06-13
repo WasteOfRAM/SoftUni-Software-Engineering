7d2aff274cda35a11dfd81fad0bee800558afcd8
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-06-13 23:31:00 +0300

    Lab solution.

---
+++ C#_Advanced_May_2022/Iterators_and_Comparators/Lab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+            this.books.Sort();
+        }
+        
+        public IEnumerator<Book> GetEnumerator()
+        {
+            //for (int i = 0; i < this.books.Count; i++)
+            //{
+            //    yield return this.books[i];
+            //}
+
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int currentIndex;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+
+            public Book Current => this.books[currentIndex];
+
+            object IEnumerator.Current => this.Current;
+
+            public void Dispose() { }
+
+            public bool MoveNext() => ++this.currentIndex < this.books.Count;
+
+            public void Reset() => this.currentIndex = -1;
+        }
+    }
+}
---
+++ C#_Advanced_May_2022/Iterators_and_Comparators/README.md
@@ -0,0 +1,5 @@
+### Problems descriptions and testing  
+
+Needs a registration to view.  
+[Lab](https://judge.softuni.org/Contests/1489/Iterators-and-Comparators-Lab)  
+[Exercise](https://judge.softuni.org/Contests/1490/Iterators-and-Comparators-Exercise)
