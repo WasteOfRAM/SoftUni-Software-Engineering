2516be7a7321d5fd834e3b0f65a2b9badb6bfb04
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-06-15 22:55:50 +0300

    Solution setup and problem 1 solution

---
+++ C#_Advanced_May_2022/Exam Preparation/CS_Advanced_Exam_20_February_2021/P01.The_Fight_for_Gondor/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.The_Fight_for_Gondor
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int waves = int.Parse(Console.ReadLine());
+
+            Queue<int> plates = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+            Stack<int> orcsWave = null;
+
+
+            for (int i = 1; i <= waves; i++)
+            {
+                if (plates.Count == 0)
+                    break;
+
+                orcsWave = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+                if (i % 3 == 0)
+                    plates.Enqueue(int.Parse(Console.ReadLine()));
+
+                while (plates.Count > 0 && orcsWave.Count > 0)
+                {
+
+                    int orkWarior = orcsWave.Pop();
+                    int plate = plates.Dequeue();
+
+                    if (orkWarior > plate)
+                    {
+                        orkWarior -= plate;
+                        orcsWave.Push(orkWarior);
+                    }
+                    else if (orkWarior < plate)
+                    {
+                        plate -= orkWarior;
+                        var newPlates = new Queue<int>();
+                        newPlates.Enqueue(plate);
+                        foreach (var item in plates)
+                        {
+                            newPlates.Enqueue(item);
+                        }
+
+                        plates = newPlates;
+                    }
+                }
+            }
+
+            if(plates.Count > 0)
+            {
+                Console.WriteLine("The people successfully repulsed the orc's attack.");
+                Console.WriteLine($"Plates left: {string.Join(", ", plates)}");
+            }
+            else
+            {
+                Console.WriteLine("The orcs successfully destroyed the Gondor's defense.");
+                Console.WriteLine($"Orcs left: {string.Join(", ", orcsWave)}");
+            }
+
+
+        }
+    }
+}
--- C#_Advanced_May_2022/Exam Preparation/README.md
+++ C#_Advanced_May_2022/Exam Preparation/README.md
@@ -6,3 +6,4 @@ Needs a registration to view.
 [C# Advanced Retake Exam - 19 August 2020](https://judge.softuni.org/Contests/2498/CSharp-Advanced-Retake-Exam-19-August-2020)  
 [C# Advanced Exam - 25 October 2020](https://judge.softuni.org/Contests/2597/CSharp-Advanced-Exam-25-October-2020)  
 [C# Advanced Retake Exam - 16 December 2020](https://judge.softuni.org/Contests/2698/CSharp-Advanced-Retake-Exam-16-December-2020)  
+[C# Advanced Exam - 20 February 2021](https://judge.softuni.org/Contests/2813/CSharp-Advanced-Exam-20-February-2021)  
