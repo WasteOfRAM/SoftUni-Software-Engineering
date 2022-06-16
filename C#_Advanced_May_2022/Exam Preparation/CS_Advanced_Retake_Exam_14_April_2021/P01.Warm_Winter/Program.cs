e9781f862d1eec7344fa59e3c65051ff37f3a4ad
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-06-16 21:09:42 +0300

    Solution setup and problem one solution

---
+++ C#_Advanced_May_2022/Exam Preparation/CS_Advanced_Retake_Exam_14_April_2021/P01.Warm_Winter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.Warm_Winter
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Stack<int> hats = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Queue<int> scarfs = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            var sets = new List<int>();
+
+
+            while (hats.Count > 0 && scarfs.Count > 0)
+            {
+                int hatPrice = hats.Peek();
+                int scarfPrice = scarfs.Peek();
+
+                if (hatPrice > scarfPrice)
+                {
+                    int set = hatPrice + scarfPrice;
+
+                    hats.Pop();
+                    scarfs.Dequeue();
+
+                    sets.Add(set);
+                }
+                else if (hatPrice < scarfPrice)
+                {
+                    hats.Pop();
+                }
+                else if (hatPrice == scarfPrice)
+                {
+                    scarfs.Dequeue();
+                    hats.Pop();
+                    hats.Push(++hatPrice);
+                }
+            }
+
+            Console.WriteLine($"The most expensive set is: {sets.Max()}");
+            Console.WriteLine(string.Join(" ", sets));
+        }
+    }
+}
--- C#_Advanced_May_2022/Exam Preparation/README.md
+++ C#_Advanced_May_2022/Exam Preparation/README.md
@@ -7,3 +7,4 @@ Needs a registration to view.
 [C# Advanced Exam - 25 October 2020](https://judge.softuni.org/Contests/2597/CSharp-Advanced-Exam-25-October-2020)  
 [C# Advanced Retake Exam - 16 December 2020](https://judge.softuni.org/Contests/2698/CSharp-Advanced-Retake-Exam-16-December-2020)  
 [C# Advanced Exam - 20 February 2021](https://judge.softuni.org/Contests/2813/CSharp-Advanced-Exam-20-February-2021)  
+[C# Advanced Retake Exam - 14 April 2021](https://judge.softuni.org/Contests/2824/CSharp-Advanced-Retake-Exam-14-April-2021)  
