70a7957527ff1f2040b075189f7c65ae9e92de0d
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-06-22 00:15:20 +0300

    Problem 3 solution

--- C#_Advanced_May_2022/Exam Preparation/CS_Advanced_Exam_23_October_2021/P03.Stock_Market/Investor.cs
+++ C#_Advanced_May_2022/Exam Preparation/CS_Advanced_Exam_23_October_2021/P03.Stock_Market/Investor.cs
@@ -1,7 +1,77 @@
-﻿namespace StockMarket
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockMarket
 {
     public class Investor
     {
-     
+        private List<Stock> stocks;
+
+        public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
+            FullName = fullName;
+            EmailAddress = emailAddress;
+            MoneyToInvest = moneyToInvest;
+            BrokerName = brokerName;
+
+            this.stocks = new List<Stock>();
+        }
+
+        public List<Stock> Portfolio { get => this.stocks; set => this.stocks = value; }
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal MoneyToInvest { get; set; }
+        public string BrokerName { get; set; }
+        public int Count => this.stocks.Count;
+
+        public void BuyStock(Stock stock)
+        {
+            if(stock.MarketCapitalization >= 10000 && this.MoneyToInvest >= stock.PricePerShare)
+            {
+                this.stocks.Add(stock);
+                MoneyToInvest -= stock.PricePerShare;
+            }
+        }
+
+        public string SellStock(string companyName, decimal sellPrice)
+        {
+            var stockTosell = stocks.FirstOrDefault(s => s.CompanyName == companyName);
+
+            if (stockTosell == null)
+                return $"{companyName} does not exist.";
+
+            if (sellPrice < stockTosell.PricePerShare)
+                return $"Cannot sell {companyName}.";
+
+            stocks.Remove(stockTosell);
+            MoneyToInvest += sellPrice;
+
+            return $"{companyName} was sold.";
+        }
+
+        public Stock FindStock(string companyName)
+        {
+            return this.stocks.FirstOrDefault(s => s.CompanyName == companyName);
+        }
+
+        public Stock FindBiggestCompany()
+        {
+            if(this.stocks.Count == 0) 
+                return null;
+
+            return this.stocks.OrderByDescending(s => s.MarketCapitalization).FirstOrDefault();
+        }
+
+        public string InvestorInformation()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"The investor {FullName} with a broker {BrokerName} has stocks:");
+            sb.Append(string.Join(Environment.NewLine, this.stocks));
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
--- C#_Advanced_May_2022/Exam Preparation/CS_Advanced_Exam_23_October_2021/P03.Stock_Market/Stock.cs
+++ C#_Advanced_May_2022/Exam Preparation/CS_Advanced_Exam_23_October_2021/P03.Stock_Market/Stock.cs
@@ -1,7 +1,33 @@
-﻿namespace StockMarket
+﻿using System.Text;
+
+namespace StockMarket
 {
     public class Stock
     {
+        public Stock(string companyName, string director, decimal pricePerShare, int totalNumberOfShares)
+        {
+            CompanyName = companyName;
+            Director = director;
+            PricePerShare = pricePerShare;
+            TotalNumberOfShares = totalNumberOfShares;
+        }
 
+        public string CompanyName { get; set; }
+        public string Director { get; set; }
+        public decimal PricePerShare { get; set; }
+        public int TotalNumberOfShares { get; set; }
+        public decimal MarketCapitalization => PricePerShare * TotalNumberOfShares;
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Company: {CompanyName}")
+                .AppendLine($"Director: {Director}")
+                .AppendLine($"Price per share: ${PricePerShare}")
+                .AppendLine($"Market capitalization: ${MarketCapitalization}");
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
