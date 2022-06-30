d6cf960ee36e7cec0e3bce663228fca1fa4ce2e9
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-06-30 23:41:38 +0300

    Setup and problems 1, 2 and 3 solutions

---
+++ C#_OOP_June_2022/Inheritance/Exercise/P01.Person/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get => this.name; set => this.name = value; }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+
+        }
+    }
+}
---
+++ C#_OOP_June_2022/Inheritance/Exercise/P03.Players_and_Monsters/Hero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        private string username;
+        private int level;
+
+        public Hero(string username, int level)
+        {
+            this.Username = username;
+            this.Level = level;
+        }
+
+        public string Username { get => this.username; set => this.username = value; }
+        public int Level { get => this.level; set => this.level = value; }
+
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}";
+        }
+    }
+}
