e7059a3ed5a7a021e7783957531633ed2deafab1
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-07-02 00:00:51 +0300

    Problem 6 solution

---
+++ C#_OOP_June_2022/Inheritance/Exercise/P06.Animals/Animal.cs
@@ -0,0 +1,68 @@
+﻿namespace Animals
+{
+    using System;
+    using System.Text;
+
+    public abstract class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        protected Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Invalid input!");
+
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Invalid input!");
+
+                this.age = value;
+            }
+        }
+
+        public string Gender
+        {
+            get { return this.gender; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Invalid input!");
+
+                this.gender = value;
+            }
+        }
+
+        public abstract string ProduceSound();
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(this.GetType().Name)
+                .AppendLine($"{this.Name} {this.Age} {this.Gender}")
+                .AppendLine(this.ProduceSound());
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#_OOP_June_2022/Inheritance/Exercise/P06.Animals/StartUp.cs
@@ -0,0 +1,51 @@
+﻿namespace Animals
+{
+    using System;
+    using System.Collections.Generic;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var animals = new List<Animal>();
+
+            Animal animal = null;
+
+            string animalType;
+            while ((animalType = Console.ReadLine()) != "Beast!")
+            {
+                string[] info = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = info[0];
+                int age = int.Parse(info[1]);
+
+                try
+                {
+                    if (animalType == "Dog")
+                        animal = new Dog(name, age, info[2]);
+                    else if (animalType == "Frog")
+                        animal = new Frog(name, age, info[2]);
+                    else if (animalType == "Cat")
+                        animal = new Cat(name, age, info[2]);
+                    else if (animalType == "Tomcat")
+                        animal = new Tomcat(name, age);
+                    else if (animalType == "Kitten")
+                        animal = new Kitten(name, age);
+                    else
+                        throw new ArgumentException("Invalid input!");
+
+                    animals.Add(animal);
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            foreach (var currAnimal in animals)
+            {
+                Console.WriteLine(currAnimal);
+            }
+        }
+    }
+}
