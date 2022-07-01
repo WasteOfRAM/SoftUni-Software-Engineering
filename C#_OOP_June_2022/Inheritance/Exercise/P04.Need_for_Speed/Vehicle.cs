3c68646269460322e51a6c44f3f651871cfd4e64
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-07-01 23:04:02 +0300

    Problems 4 and 5 solutions

---
+++ C#_OOP_June_2022/Inheritance/Exercise/P04.Need_for_Speed/Vehicle.cs
@@ -0,0 +1,22 @@
+﻿namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            Fuel = fuel;
+            HorsePower = horsePower;
+        }
+
+        public virtual double FuelConsumption { get => DefaultFuelConsumption; }
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            this.Fuel -= this.FuelConsumption * kilometers;
+        }
+    }
+}
