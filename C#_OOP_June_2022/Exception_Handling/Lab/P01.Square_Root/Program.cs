b8d2b4def0cf79c2701b02e8974c7d7e9aec6dc6
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-07-15 00:31:25 +0300

    Setup and problems 1 and 2 solutions

---
+++ C#_OOP_June_2022/Exception_Handling/Lab/P01.Square_Root/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace P01.Square_Root
+{
+    using System;
+    internal class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            try
+            {
+                if (number < 0)
+                    throw new ArgumentException("Invalid number.");
+
+                int sqereRoot = (int)Math.Sqrt(number);
+
+                Console.WriteLine(sqereRoot);
+            }
+            catch (ArgumentException msg)
+            {
+                Console.WriteLine(msg.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
---
+++ C#_OOP_June_2022/Exception_Handling/Lab/P02.Enter_Numbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02.Enter_Numbers
+{
+    internal class Program
+    {
+        private static HashSet<int> numbers = new HashSet<int>();
+        static void Main()
+        {
+            ReadNumber(1, 100);
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+
+        private static void ReadNumber(int start, int end)
+        {
+            while (numbers.Count < 10)
+            {
+                string input = Console.ReadLine();
+                int number;
+
+                try
+                {
+                    if (!int.TryParse(input, out number))
+                        throw new ArgumentException("Invalid Number!");
+
+                    if (number > start && number < end)
+                    {
+                        numbers.Add(number);
+                        start = number;
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"Your number is not in range {start} - {end}!");
+                    }
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
