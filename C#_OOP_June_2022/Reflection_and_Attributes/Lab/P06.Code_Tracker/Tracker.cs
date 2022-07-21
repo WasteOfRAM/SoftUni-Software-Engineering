9a0411927d2e356252d9e42fbae58f507213addd
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-07-22 00:37:23 +0300

    problems 5 and 6

---
+++ C#_OOP_June_2022/Reflection_and_Attributes/Lab/P06.Code_Tracker/Tracker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+
+            var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static);
+
+            foreach (var method in methods)
+            {
+                if(method.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(typeof(AuthorAttribute), true);
+                    foreach (AuthorAttribute attr in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attr.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
