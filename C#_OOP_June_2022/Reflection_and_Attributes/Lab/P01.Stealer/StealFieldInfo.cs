d1a7d694f360f0d4a0a88d963bd32f7c23820e58
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-07-21 23:06:39 +0300

    Setup and problem 1 solution

---
+++ C#_OOP_June_2022/Reflection_and_Attributes/Lab/P01.Stealer/StealFieldInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public Spy()
+        {
+        }
+
+        public string StealFieldInfo(string className, params string[] fields)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type hackeAccount = Type.GetType(className);
+            FieldInfo[] fieldInfos = hackeAccount.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            var classInstance = Activator.CreateInstance(hackeAccount);
+
+            sb.AppendLine($"Class under investigation: {hackeAccount}");
+
+            foreach (var field in fieldInfos.Where(f => fields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
