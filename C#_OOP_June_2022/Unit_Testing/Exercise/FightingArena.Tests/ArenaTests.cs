c1b0cc534b40a376297c542f72c18a2815990700
WasteOfRAM <43553573+WasteOfRAM@users.noreply.github.com>
2022-07-28 23:13:16 +0300

    Fighting arena solution

--- C#_OOP_June_2022/Unit_Testing/Exercise/FightingArena.Tests/ArenaTests.cs
+++ C#_OOP_June_2022/Unit_Testing/Exercise/FightingArena.Tests/ArenaTests.cs
@@ -1,10 +1,139 @@
 ﻿namespace FightingArena.Tests
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using NUnit.Framework;
 
     [TestFixture]
     public class ArenaTests
     {
+        [Test]
+        public void ConstructorInitializesEmptyCollectionOfWarriors()
+        {
+            // Arrange
+            var arena = new Arena();
+            var expectedCollection = new List<Warrior>();
+            var actualCollection = arena.Warriors.ToList();
 
+            CollectionAssert.AreEqual(expectedCollection, actualCollection);
+        }
+
+        [Test]
+        public void TestIfEnrollMethodAddsCorrectlyUniqueWarriors()
+        {
+            // Arrange
+            var arena = new Arena();
+            var warrior = new Warrior("warror1", 50, 50);
+            var warrior2 = new Warrior("warror2", 50, 50);
+            var warrior3 = new Warrior("warrior3", 50, 50);
+
+            var expectedCollection = new List<Warrior> { warrior, warrior2, warrior3 };
+
+            // Act
+            arena.Enroll(warrior);
+            arena.Enroll(warrior2);
+            arena.Enroll(warrior3);
+
+            var actualCollection = arena.Warriors.ToList();
+
+            // Assert
+            CollectionAssert.AreEqual(expectedCollection, actualCollection);
+        }
+
+        [Test]
+        public void TestIfCountGetterReturnsCollectionSize()
+        {
+            // Arrange
+            var arena = new Arena();
+            for (int i = 1; i <= 3; i++)
+            {
+                arena.Enroll(new Warrior("name" + i, 50, 50));
+            }
+
+            int expectedCount = 3;
+
+            // Act
+            int actualCount = arena.Count;
+
+            // Assert
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [Test]
+        public void EnrollMethodThrowsExeptionIfWarriorEnrollsSecondTime()
+        {
+            // Arrange
+            var arena = new Arena();
+            var warrior = new Warrior("warror", 50, 50);
+            var warrior2 = new Warrior("warror", 50, 50);
+            arena.Enroll(warrior);
+
+            // Act & Asset
+            Assert.That(() =>
+            {
+                arena.Enroll(warrior2);
+            }, Throws.Exception.TypeOf<InvalidOperationException>(), "Warrior is already enrolled for the fights!");
+        }
+
+        [Test]
+        public void FightMethodTestIfTheFightHappend()
+        {
+            // Arrange
+            var arena = new Arena();
+            var warriorA = new Warrior("warrorA", 50, 80);
+            var warriorD = new Warrior("warrorD", 45, 80);
+
+            arena.Enroll(warriorA);
+            arena.Enroll(warriorD);
+
+            // Act
+            arena.Fight("warrorA", "warrorD");
+
+            var warriorAExpectedHp = 80 - warriorD.Damage;
+            var warriorDExpectedHp = 80 - warriorA.Damage;
+
+            // Assert
+            Assert.AreEqual(warriorAExpectedHp, warriorA.HP);
+            Assert.AreEqual(warriorDExpectedHp, warriorD.HP);
+        }
+
+        [Test]
+        public void FightMethodIfAttackerNameIsNull()
+        {
+            //Arange
+            var arena = new Arena();
+            var warriorA = new Warrior("warrorA", 50, 80);
+            var warriorD = new Warrior("warrorD", 45, 80);
+            var warriorInvalid = new Warrior("warriorInvalid", 45, 80);
+
+            arena.Enroll(warriorA);
+            arena.Enroll(warriorD);
+
+            // Act & Assert
+            Assert.That(() =>
+            {
+                arena.Fight(warriorInvalid.Name, warriorD.Name);
+            }, Throws.Exception.TypeOf<InvalidOperationException>(), $"There is no fighter with name {warriorInvalid.Name} enrolled for the fights!");
+        }
+
+        [Test]
+        public void FightMethodDefenderNameIsNull()
+        {
+            //Arange
+            var arena = new Arena();
+            var warriorA = new Warrior("warrorA", 50, 80);
+            var warriorD = new Warrior("warrorD", 45, 80);
+            var warriorInvalid = new Warrior("warriorInvalid", 45, 80);
+
+            arena.Enroll(warriorA);
+            arena.Enroll(warriorD);
+
+            // Act & Assert
+            Assert.That(() =>
+            {
+                arena.Fight(warriorA.Name, warriorInvalid.Name);
+            }, Throws.Exception.TypeOf<InvalidOperationException>(), $"There is no fighter with name {warriorInvalid.Name} enrolled for the fights!");
+        }
     }
 }
--- C#_OOP_June_2022/Unit_Testing/Exercise/FightingArena.Tests/WarriorTests.cs
+++ C#_OOP_June_2022/Unit_Testing/Exercise/FightingArena.Tests/WarriorTests.cs
@@ -1,10 +1,204 @@
 namespace FightingArena.Tests
 {
+    using System;
+    using System.Linq;
+    using System.Reflection;
     using NUnit.Framework;
 
     [TestFixture]
     public class WarriorTests
     {
+        [Test]
+        public void ConstructorIsWorking()
+        {
+            // Arrange
+            string expectedName = "Anbatoli";
+            int expextedDamage = 50;
+            int expectedHp = 100;
 
+            // Act
+            var warrior = new Warrior(expectedName, expextedDamage, expectedHp);
+
+            // Assert
+            //Assert.AreEqual(expectedName, warior.Name);
+            //Assert.AreEqual(expextedDamage, warior.Damage);
+            //Assert.AreEqual(expectedHp, warior.HP);
+
+            FieldInfo nameField = this.GetField("name");
+            string actualName = (string)nameField.GetValue(warrior);
+
+            FieldInfo damageField = this.GetField("damage");
+            int actualDamage = (int)damageField.GetValue(warrior);
+
+            FieldInfo hpField = this.GetField("hp");
+            int actualHp = (int)hpField.GetValue(warrior);
+
+            Assert.AreEqual(expectedName, actualName);
+            Assert.AreEqual(expextedDamage, actualDamage);
+            Assert.AreEqual(expectedHp, actualHp);
+        }
+
+        [Test]
+        public void TestNameGetter()
+        {
+            // Arrange
+            string expectedName = "Anbatoli";
+            int expextedDamage = 50;
+            int expectedHp = 100;
+            var warrior = new Warrior(expectedName, expextedDamage, expectedHp);
+
+            // Act
+            var actualName = warrior.Name;
+
+            // Assert
+            Assert.AreEqual(expectedName, actualName);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("         ")]
+        public void TestWarriorNameSetterWithInvalidInput(string invalidName)
+        {
+            // Act & Assert
+            Assert.That(() =>
+            {
+                var warior = new Warrior(invalidName, 10, 10);
+            }, Throws.Exception.TypeOf<ArgumentException>(), "Name should not be empty or whitespace!");
+        }
+
+        [Test]
+        public void TestWarriorDamageGetter()
+        {
+            // Arrange
+            string expectedName = "Anbatoli";
+            int expextedDamage = 50;
+            int expectedHp = 100;
+            var warrior = new Warrior(expectedName, expextedDamage, expectedHp);
+
+            // Act
+            var actualDamage = warrior.Damage;
+
+            // Assert
+            Assert.AreEqual(expextedDamage, actualDamage);
+        }
+
+        [TestCase(0)]
+        [TestCase(-14)]
+        public void TestWarriorDamageSetterWithNegativeNumberAndZero(int invalidDamage)
+        {
+            // Act & Assert
+            Assert.That(() =>
+            {
+                var warior = new Warrior("Anbatoli", invalidDamage, 10);
+            }, Throws.Exception.TypeOf<ArgumentException>(), "Damage value should be positive!");
+        }
+
+        [Test]
+        public void TestWarriorHPGetter()
+        {
+            // Arrange
+            string expectedName = "Anbatoli";
+            int expextedDamage = 50;
+            int expectedHp = 100;
+            var warrior = new Warrior(expectedName, expextedDamage, expectedHp);
+
+            // Act
+            var actualHP = warrior.HP;
+
+            // Assert
+            Assert.AreEqual(expectedHp, actualHP);
+        }
+
+        [Test]
+        public void TestWarriorHPSetterWithNegativeValue()
+        {
+            // Act & Assert
+            Assert.That(() =>
+            {
+                var warior = new Warrior("Anbatoli", 10, -14);
+            }, Throws.Exception.TypeOf<ArgumentException>(), "HP should not be negative!");
+        }
+
+        [TestCase(30)]
+        [TestCase(1)]
+        public void TestAttackWithHPBelowMinMinAttackHP(int attackingWariorHP)
+        {
+            // Arrange
+            var attacingWarior = new Warrior("name", 50, attackingWariorHP);
+            var defendingWarrior = new Warrior("name?", 1, 50);
+
+            // Act & Assert
+            Assert.That(() =>
+            {
+                attacingWarior.Attack(defendingWarrior);
+            }, Throws.Exception.TypeOf<InvalidOperationException>(), "Your HP is too low in order to attack other warriors!");
+        }
+
+        [TestCase(30)]
+        [TestCase(1)]
+        public void TestAttackingAnEnemyWithHPBelowMinAttackHP(int deffendingWarriorHP)
+        {
+            // Arrange
+            var attacingWarior = new Warrior("name", 50, 50);
+            var defendingWarrior = new Warrior("name?", 1, deffendingWarriorHP);
+
+            FieldInfo minAttackHPField = typeof(Warrior).GetFields(BindingFlags.Static | BindingFlags.NonPublic).First(field => field.Name == "MIN_ATTACK_HP");
+            var minAttackHP = (int)minAttackHPField.GetValue(null);
+
+            // Act & Assert
+            Assert.That(() =>
+            {
+                attacingWarior.Attack(defendingWarrior);
+            }, Throws.Exception.TypeOf<InvalidOperationException>(), $"Enemy HP must be greater than {minAttackHP} in order to attack him!");
+        }
+
+        [Test]
+        public void TestAttackingAndDefendingWarriorWithDamageBigerThanAttackingWarriorHP()
+        {
+            // Arrange
+            var attacingWarior = new Warrior("name", 50, 35);
+            var defendingWarrior = new Warrior("name?", 40, 50);
+
+            // Act & Assert
+            Assert.That(() =>
+            {
+                attacingWarior.Attack(defendingWarrior);
+            }, Throws.Exception.TypeOf<InvalidOperationException>(), $"You are trying to attack too strong enemy");
+        }
+
+        [Test]
+        public void TestIfAttackingWarriorHPGoesDownOnAttack()
+        {
+            // Arrange
+            var attacingWarior = new Warrior("name", 50, 55);
+            var defendingWarrior = new Warrior("name?", 50, 50);
+
+            var expectedHP = attacingWarior.HP - defendingWarrior.Damage;
+
+            // Act
+            attacingWarior.Attack(defendingWarrior);
+
+            // Assert
+            Assert.AreEqual(expectedHP, attacingWarior.HP);
+        }
+
+        [Test]
+        public void TestIfDefendingWarriorHpIsSetToZeroIfKilled()
+        {
+            // Arrange
+            var attacingWarior = new Warrior("name", 50, 55);
+            var defendingWarrior = new Warrior("name?", 45, 45);
+
+            // Act
+            attacingWarior.Attack(defendingWarrior);
+
+            // Assert
+            Assert.AreEqual(0, defendingWarrior.HP);
+        }
+
+        private FieldInfo GetField(string fieldName) 
+            => typeof(Warrior)
+            .GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+            .First(field => field.Name == fieldName);
     }
 }
