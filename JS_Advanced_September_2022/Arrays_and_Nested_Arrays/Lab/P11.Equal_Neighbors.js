ee00447cf55148d3c8c378997f092ab4e9b2e4b9
Ivan Kukutsov <ivankukucov@gmail.com>
2022-09-15 23:53:57 +0300

    Update P11.Equal_Neighbors.js

--- JS_Advanced_September_2022/Arrays_and_Nested_Arrays/Lab/P11.Equal_Neighbors.js
+++ JS_Advanced_September_2022/Arrays_and_Nested_Arrays/Lab/P11.Equal_Neighbors.js
@@ -1,14 +1,7 @@
 function equalNeighbors(matrix) {
     let neighborsCount = 0;
 
-    for (let row = 0; row < matrix.length; row++) {
-
-        for (let col = 0; col < matrix[row].length; col++) {
-            if(matrix[row][col] === matrix[row][col + 1] || matrix[row][col] === matrix[row + 1][col]){
-                neighborsCount++;
-            }
-        }
-    }
+    
 
     return neighborsCount;
 }
