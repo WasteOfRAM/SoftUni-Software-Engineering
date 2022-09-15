bdf818cf78f7209c6cbea6ec3a39b593906163e1
Ivan Kukutsov <ivankukucov@gmail.com>
2022-09-16 00:13:28 +0300

    problem 11 solution

--- JS_Advanced_September_2022/Arrays_and_Nested_Arrays/Lab/P11.Equal_Neighbors.js
+++ JS_Advanced_September_2022/Arrays_and_Nested_Arrays/Lab/P11.Equal_Neighbors.js
@@ -1,7 +1,30 @@
 function equalNeighbors(matrix) {
     let neighborsCount = 0;
 
-    
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[row].length; col++) {
+            if (row < matrix.length - 1 && col < matrix[row].length - 1) {
+                if (matrix[row][col] === matrix[row + 1][col]) {
+                    neighborsCount++;
+                }
+
+                if (matrix[row][col] === matrix[row][col + 1]) {
+                    neighborsCount++;
+                }
+            } else {
+                if (row === matrix.length - 1) {
+                    if (matrix[row][col] === matrix[row][col + 1]) {
+                        neighborsCount++;
+                    }
+                } else if (col === matrix[row].length - 1) {
+                    if (matrix[row][col] === matrix[row + 1][col]) {
+                        neighborsCount++;
+                    }
+                }
+            }
+        }
+        
+    }
 
     return neighborsCount;
 }
