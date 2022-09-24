da223200cbc59f77b881462fb98d61230801ca2c
Ivan Kukutsov <ivankukucov@gmail.com>
2022-09-24 22:41:11 +0300

    problems 7, 8 solutions

--- JS_Advanced_September_2022/DOM_Introduction/Exercise/P07.Hell's_Kitchen/app.js
+++ JS_Advanced_September_2022/DOM_Introduction/Exercise/P07.Hell's_Kitchen/app.js
@@ -1,8 +1,53 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
+   function onClick() {
+      let input = document.querySelector("#inputs textarea").value;
+      let bestResturantParagraph = document.querySelector("#bestRestaurant p");
+      let bestResturantWorkersParagraph = document.querySelector("#workers p");
+
+      input = JSON.parse(input);
+      let resturants = [];
+
+      for (const resturantData of input) {
+         let [resturantName, workers] = resturantData.split(' - ');
+         workers = workers.split(', ')
+            .map(worker => {
+               let [name, salary] = worker.split(' ');
+               return { name: name, salary: Number(salary) }
+            });
+
+         if (resturants.find(r => r.name === resturantName)) {
+            let currentResturant = resturants.find(r => r.name === resturantName);
+            currentResturant.workerList = currentResturant.workerList.concat(workers);
+            continue;
+         }
+
+         resturants.push({
+            name: resturantName,
+            workerList: workers,
+            averageSalary: function () {
+               let ava = this.workerList.reduce((a, w) => a + w.salary, 0) / this.workerList.length
+               return ava.toFixed(2);
+            },
+            bestSalary: function () {
+               return this.workerList.sort((a, b) => b.salary - a.salary)[0].salary.toFixed(2);
+            },
+            sortedWorkers: function () {
+               return this.workerList.sort((a, b) => b.salary - a.salary);
+            }
+         });
+
+      }
+
+      let bestResturant = resturants.sort((a, b) => b.averageSalary() - a.averageSalary())[0];
+      bestResturantParagraph.textContent = `Name: ${bestResturant.name} Average Salary: ${bestResturant.averageSalary()} Best Salary: ${bestResturant.bestSalary()}`;
+      let result = '';
+      let workers = bestResturant.sortedWorkers();
+      for (const worker of workers) {
+         result += `Name: ${worker.name} With Salary: ${worker.salary} `;
+      }
+
+      bestResturantWorkersParagraph.textContent = result;
    }
 }
--- JS_Advanced_September_2022/DOM_Introduction/Exercise/P08.Generate_Report/report.js
+++ JS_Advanced_September_2022/DOM_Introduction/Exercise/P08.Generate_Report/report.js
@@ -1,3 +1,28 @@
 function generateReport() {
-    //TODO
+    let output = document.getElementById("output");
+    let checkBoxList = document.querySelectorAll('input[type=checkbox]');
+    checkBoxList = Array.from(checkBoxList);
+    let rowsData = document.querySelectorAll("tbody tr");
+    let resultArray = [];
+    let selected = [];
+    
+    for (let index = 0; index < checkBoxList.length; index++) {
+        if(checkBoxList[index].checked){
+            selected.push(index);
+        }
+    }
+    
+    for (const row of rowsData) {
+        let obj = {};
+
+        for (const index of selected) {
+            obj[checkBoxList[index].name] = row.cells[index].textContent;
+        }
+
+        resultArray.push(obj);
+    }
+
+    let jsonString = JSON.stringify(resultArray);
+
+    output.textContent = jsonString;
 }
