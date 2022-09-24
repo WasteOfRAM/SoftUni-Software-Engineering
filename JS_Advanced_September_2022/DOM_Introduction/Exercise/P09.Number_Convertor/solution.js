a96d1cc5cc6b422b03988f6f3738c147a953fbb5
Ivan Kukutsov <ivankukucov@gmail.com>
2022-09-24 23:17:19 +0300

    Problem 9 solution

--- JS_Advanced_September_2022/DOM_Introduction/Exercise/P09.Number_Convertor/solution.js
+++ JS_Advanced_September_2022/DOM_Introduction/Exercise/P09.Number_Convertor/solution.js
@@ -1,4 +1,37 @@
 function solve() {
+    document.querySelector("button").addEventListener('click', onClick);
+    
+    let result = document.getElementById('result');
 
-    //TODO...
+    let selectTo = document.getElementById("selectMenuTo");
+    let binary = document.createElement("option");
+    binary.value = 'binary';
+    binary.textContent = 'Binary';
+
+    let hexadecimal = document.createElement("option");
+    hexadecimal.value = 'hexadecimal';
+    hexadecimal.textContent = 'Hexadecimal';
+
+    selectTo.appendChild(binary);
+    selectTo.appendChild(hexadecimal);
+
+
+    function onClick() {
+        let input = Number(document.getElementById("input").value);
+        let selectedTo = selectTo.value;
+        let conversionResult = '';
+
+        switch (selectedTo) {
+            case 'binary':
+                conversionResult = input.toString(2);
+                break;
+            case 'hexadecimal':
+                conversionResult = input.toString(16);
+                break;
+            default:
+                break;
+        }
+
+        result.value = conversionResult.toUpperCase();
+    }
 }
