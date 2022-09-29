33c0500d2cd30c0f7912b03e98581e6907e3f5b1
Ivan Kukutsov <ivankukucov@gmail.com>
2022-09-29 22:17:30 +0300

    Problems 1, 2 and 3 solutions

---
+++ JS_Advanced_September_2022/Advanced_Functions/Lab/P02.Add.js
@@ -0,0 +1,15 @@
+function solution(number) {
+    let numbetToAdd = number;
+
+    return (n) => n + numbetToAdd;
+}
+
+let add5 = solution(5);
+console.log(add5(2));
+console.log(add5(3));
+
+console.log('---------------');
+
+let add7 = solution(7);
+console.log(add7(2));
+console.log(add7(3));
